68a563035109d4064fb0153fefe668cb26a1e17a
Bilel <maamouri.bilel@gmail.com>
2019-08-19 15:11:35 +0100

    Added print pdf file function after finishing the order

--- src/app/client-side/order3/order3.component.ts
+++ src/app/client-side/order3/order3.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
+import * as jsPDF from 'jspdf';
+import domtoimage from 'dom-to-image';
 
 @Component({
   selector: 'app-order3',
@@ -7,7 +9,9 @@ import { Router } from '@angular/router';
   styleUrls: ['./order3.component.css']
 })
 export class Order3Component implements OnInit {
+  @ViewChild('invoice', { static: false }) invoice: ElementRef;
   orderDetails: any;
+
   constructor(private router: Router) {
     if (JSON.parse(localStorage.getItem('orderDetails')) != null) {
       this.orderDetails = JSON.parse(localStorage.getItem('orderDetails'));
@@ -18,4 +22,46 @@ export class Order3Component implements OnInit {
   }
 
   ngOnInit() {}
+
+  makePdf() {
+    const node = document.getElementById('invoice');
+
+    let img;
+    let filename;
+    let newImage;
+
+    domtoimage
+      .toPng(node, { bgcolor: '#fff' })
+
+      .then(dataUrl => {
+        img = new Image();
+        img.src = dataUrl;
+        newImage = img.src;
+
+        img.onload = () => {
+          const pdfWidth = img.width;
+          const pdfHeight = img.height;
+
+          // FileSaver.saveAs(dataUrl, 'my-pdfimage.png'); // Save as Image
+
+          let doc;
+
+          if (pdfWidth > pdfHeight) {
+            doc = new jsPDF('l', 'px', [pdfWidth, pdfHeight]);
+          } else {
+            doc = new jsPDF('p', 'px', [pdfWidth, pdfHeight]);
+          }
+
+          const width = doc.internal.pageSize.getWidth();
+          const height = doc.internal.pageSize.getHeight();
+
+          doc.addImage(newImage, 'PNG', 10, 10, width, height);
+          filename = 'recu_de_paiement_' + this.orderDetails.nom + '.pdf';
+          doc.save(filename);
+        };
+      })
+      .catch(error => {
+        // Error Handling
+      });
+  }
 }
